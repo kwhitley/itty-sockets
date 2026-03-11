45e9148db412e74e298f0880c9f70dccc7d9eab1
Kevin Whitley <krwhitley@gmail.com>
2026-03-11 10:15:25 -0500

    commented out ts examples

--- src/connect.ts
+++ src/connect.ts
@@ -114,32 +114,32 @@ export let connect: IttySocketConnect = (channelId: string, options = {}) => {
   return socket
 }
 
-type MyEvents = {
-  'player-join': { playerId: string, team: string }
-  'player-leave': { playerId: string }
-  'chat': { text: string, user: string }
-  'message': number[] | Record<string, any>
-}
+// type MyEvents = {
+//   'player-join': { playerId: string, team: string }
+//   'player-leave': { playerId: string }
+//   'chat': { text: string, user: string }
+//   'message': number[] | Record<string, any>
+// }
 
-const external = connect<MyEvents>('wss://test')
-  .on('player-join', ({ playerId, team }) => {})
-  .send({ playerId: '123', team: 'Red' })
-  .on('message', (e) => e.message)
+// const external = connect<MyEvents>('wss://test')
+//   .on('player-join', ({ playerId, team }) => {})
+//   .send({ playerId: '123', team: 'Red' })
+//   .on('message', (e) => e.message)
 
-const itty = connect<MyEvents>('my-channel')
-  .on('chat', ({ text, user, date, uid }) => {})
-  .on('leave', ({ users }) => {})
-  .send({ type: 'chat', text: 'Hello', user: 'John' })
-  .send([1,2,3])
+// const itty = connect<MyEvents>('my-channel')
+//   .on('chat', ({ text, user, date, uid }) => {})
+//   .on('leave', ({ users }) => {})
+//   .send({ type: 'chat', text: 'Hello', user: 'John' })
+//   .send([1,2,3])
 
-const raw = connect('my-channel2')
-  .on('chat', ({ text, user, date, uid }) => {})
-  .on('leave', ({ users }) => {})
-  .send({ type: 'chat', text: 'Hello', user: 'John' })
-  .send([1,2,3])
+// const raw = connect('my-channel2')
+//   .on('chat', ({ text, user, date, uid }) => {})
+//   .on('leave', ({ users }) => {})
+//   .send({ type: 'chat', text: 'Hello', user: 'John' })
+//   .send([1,2,3])
 
-const localTypes = connect('my-channel2')
-  .on<{ type: 'chat', text: string, user: string }>('chat', ({ text, user, date, uid }) => {})
-  .on('leave', ({ users }) => {})
-  .send<{ type: 'chat', text: string, user: string }>({ type: 'chat', text: 'Hello', user: 'John' })
-  .send<number[]>([1,2,3])
+// const localTypes = connect('my-channel2')
+//   .on<{ type: 'chat', text: string, user: string }>('chat', ({ text, user, date, uid }) => {})
+//   .on('leave', ({ users }) => {})
+//   .send<{ type: 'chat', text: string, user: string }>({ type: 'chat', text: 'Hello', user: 'John' })
+//   .send<number[]>([1,2,3])
